558e379a95f8f597086524d780da61ba244c7dec
FranciscoKonan <92207765+FranciscoKonan@users.noreply.github.com>
2026-03-20 18:57:05 +0000

    Add configuration for Supabase connection

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const CONFIG = {
+    SUPABASE_URL: 'https://vzrufmelftbqpsemnjbd.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6cnVmbWVsZnRicXBzZW1uamJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNzYwNTMsImV4cCI6MjA4NjY1MjA1M30.1NPN666Lt9WZHupvp_XIFu-SnsaextHH_JvXgQPtyV0'
+};
+
+window.CONFIG = CONFIG;
